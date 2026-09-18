2f6221deaf839f8e304370255419322ac6613d18
HoangNguyen1306 <hmn39@case.edu>
2026-09-18 16:13:32 -0400

    Adjust pulse width and random angle limits

--- Lab_4/src/Servo Motor Random.cpp
+++ Lab_4/src/Servo Motor Random.cpp
@@ -14,28 +14,28 @@ int randomDelay;
 int pulseWidth;
 
 // Define the minimum and maximum pulse widths for the servo
-const int minPulseWidth = 500; // 0.5 ms
-const int maxPulseWidth = 2500; // 2.5 ms
+const int minPulseWidth = 1000; // 0.5 ms
+const int maxPulseWidth = 2000; // 2.5 ms
 
 void setup() {
   // Attach the servo to the specified pin and set its pulse width range
   myServo.attach(servoPin, minPulseWidth, maxPulseWidth);
 
   // Set the PWM frequency for the servo
-  myServo.setPeriodHertz(50); // Standard 50Hz servo
+  myServo.setPeriodHertz(100); // Standard 50Hz servo
 }
 
 void loop() {
   //  --- SECTION 1: Make a Random Angle Between 0 to 180 ---
   // randomAngle = ?; // random(A,B); returns a random value between A and B
-  randomAngle = random(0, 180);
+  randomAngle = random(0, 1135);
 
   // ---SECTION 2: Map Pulse Width with Angle
   // pulseWidth = map(?, ?, ?, ?, ?, ?) // from Servo Motor.cpp, what did you learn from using map function?
-  pulseWidth = map(randomAngle, 0, 180, minPulseWidth, maxPulseWidth);
+  pulseWidth = map(randomAngle, 0, 135, minPulseWidth, maxPulseWidth);
   myServo.writeMicroseconds(pulseWidth); // writing pulse width to servo
 
   // ---Random delay between 50 to 1000 milliseconds
-  randomDelay = random(50, 1000);
+  randomDelay = random(50, 2000);
   delay(randomDelay); // change delay to your own preference
 }
